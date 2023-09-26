848bf2e9b836610a7c5954d29de715f88ccced18
f5guang <fuguang23@163.com>
2023-09-26 10:01:43 +0800

    初始化项目

---
+++ src/main/java/com/yang/Main.java
@@ -0,0 +1,12 @@
+package com.yang;
+
+/**
+ * @Description: ${description}
+ * @Author : wy
+ * @Date : Created 2023/9/26 9:52 AM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
